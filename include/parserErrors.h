18b2b0d061c6becb2f295e72d96ef22c35043ef5
khbence <horvathb94@gmail.com>
2023-05-08 18:28:32 +0200

    Using fmt

--- include/parserErrors.h
+++ include/parserErrors.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <exception>
 #include <string>
+#include "fmt/core.h"
 
 namespace pparser {
     class parserError : public std::exception {
@@ -14,29 +15,29 @@ namespace pparser {
     };
 
     struct tooMuchArguments : public parserError {
-        explicit tooMuchArguments(const std::string& value) : parserError(value + " cannot bind to any parameter, because the previous object is a value!\n") {}
+        explicit tooMuchArguments(const std::string& value) : parserError(fmt::format("{} cannot bind to any parameter, because the previous object is a value!\n", value)) {}
     };
 
     struct badFormatLongArgument : public parserError {
-        explicit badFormatLongArgument(const std::string& value) : parserError("Long parameters need two hyphens (" + value + " is wrong)\n") {}
+        explicit badFormatLongArgument(const std::string& value) : parserError(fmt::format("Long parameters need two hyphens ({} is wrong)\n", value)) {}
     };
 
     struct sameLongAndShortParameters : public parserError {
-        sameLongAndShortParameters(char s, std::string& l) : parserError("-" + std::string(1, s) + " and " + "--" + l + " describes the same parameters don't use them both!\n") {}
+        sameLongAndShortParameters(char s, std::string& l) : parserError(fmt::format("-{} and --{} describes the same parameters don't use them both!\n", s, l)) {}
     };
 
     struct missingArgument : public parserError {
-        explicit missingArgument(std::string& l) : parserError("--" + l + " requires an argument!\n") {}
-        explicit missingArgument(char s) : parserError("-" + std::string(1, s) + " requires an argument!\n") {}
+        explicit missingArgument(std::string& l) : parserError(fmt::format("--{} requires an argument!\n", l)) {}
+        explicit missingArgument(char s) : parserError(fmt::format("-{} requires an argument!\n", s)) {}
     };
 
     struct unnecessaryArgument : public parserError {
-        explicit unnecessaryArgument(std::string& l) : parserError("--" + l + " does not requires any argument!\n") {}
-        explicit unnecessaryArgument(char s) : parserError("-" + std::string(1, s) + " does not requires any argument!\n") {}
+        explicit unnecessaryArgument(std::string& l) : parserError(fmt::format("--{} not requires an argument!\n", l)) {}
+        explicit unnecessaryArgument(char s) : parserError(fmt::format("-{} not requires an argument!\n", s)) {}
     };
 
     struct missingParameter : public parserError {
-        missingParameter(char s, std::string& l) : parserError("-" + std::string(1, s) + " or " + "--" + l + " need to be defined!\n") {};
-        explicit missingParameter(std::string& l) : parserError("--" + l + " need to be defined!\n") {};
+        missingParameter(char s, std::string& l) : parserError(fmt::format("-{} or --{} need to be defined!\n", s, l)) {};
+        explicit missingParameter(std::string& l) : parserError(fmt::format("--{} need to be defined!\n", l)) {};
     };
 }
